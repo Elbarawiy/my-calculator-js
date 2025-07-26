6204dfbcc504f3c29f528f3d6cd0d6587d13ff2b
Elbarawiy <ilyasuabdullahibari20@gmail.com>
2025-07-25 22:51:15 -0700

    my script of my calculator file

---
+++ script.js
@@ -0,0 +1,17 @@
+function appendTodisplay(value){
+    document.getelentById("display").value= value;
+}
+
+function clearDisplay(){
+    document.getElementById("display"). value = "";
+    }
+    function calculate(){
+       try {
+        const result=eval(document.getElementById("display").value = result;
+
+        }catch (e) {
+            alert( "invalid expression");
+        }
+        
+    }
+
